089206affea2b1f64bab1b2260b4ad6c6ffdd351
Ryan Hirsch <Ryan.Hirsch@gmail.com>
2014-01-21 19:07:45 -0600

    Initial commit for http://coding.smashingmagazine.com/2013/11/07/an-in-depth-introduction-to-ember-js/

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+mkdir -p scripts/
+mkdir -p scripts/components/
+mkdir -p scripts/controllers/
+mkdir -p scripts/helpers/
+mkdir -p scripts/models/
+mkdir -p scripts/routes/
+mkdir -p scripts/templates/
+mkdir -p scripts/views/
+
+touch scripts/app.js
+touch scripts/router.js
+touch scripts/store.js
