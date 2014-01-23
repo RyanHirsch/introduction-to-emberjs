420db9b880d44e0cdb61aba8db9c044171dc3f52
Ryan Hirsch <Ryan.Hirsch@gmail.com>
2014-01-22 19:21:17 -0600

    Delete a user

--- scripts/controllers/userController.js
+++ scripts/controllers/userController.js
@@ -1,7 +1,20 @@
 App.UserController = Ember.ObjectController.extend({
+    deleteMode: false,
     actions: {
         edit: function() {
             this.transitionToRoute('user.edit');
+        },
+        confirmDelete: function() {
+            this.get('model').deleteRecord();
+            this.get('model').save();
+            this.transitionToRoute('users');
+            this.set('deleteMode', false);
+        },
+        cancelDelete: function() {
+            this.set('deleteMode', false);
+        },
+        delete: function() {
+            this.toggleProperty('deleteMode');
         }
     }
 });
