76f8373f14214b903d3c154d1f59dff46b797842
Ryan Hirsch <Ryan.Hirsch@gmail.com>
2014-01-22 18:46:26 -0600

    Pull dependencies locally, was having network issues :(

--- setup.sh
+++ setup.sh
@@ -7,7 +7,16 @@ mkdir -p scripts/models/
 mkdir -p scripts/routes/
 mkdir -p scripts/templates/
 mkdir -p scripts/views/
+mkdir -p vendor/
 
 touch scripts/app.js
 touch scripts/router.js
 touch scripts/store.js
+
+
+wget http://code.jquery.com/jquery-2.0.3.min.js
+wget http://builds.emberjs.com/handlebars-1.0.0.js
+wget http://cdnjs.cloudflare.com/ajax/libs/ember.js/1.2.1/ember.js
+wget http://builds.emberjs.com/tags/v1.0.0-beta.3/ember-data.js
+
+mv *.js vendor/
